100f2b3c9353d34eba4ac7acbbc967a52e271889
MJB <matt.berti13@gmail.com>
2022-03-20 12:03:45 -0500

    created form input

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var searchBar = document.querySelector("#city");
+
+//add event listener to search bar to find the input text
+var getCityInfo = function(city){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,minutely,alerts&appid=1ee273d29337b74fa7d7a2d9da75ae71";
+
+
+}
