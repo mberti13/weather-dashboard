144dc521fb1c5db6b1ab2cfb734b15eab49d62ce
MJB <matt.berti13@gmail.com>
2022-03-30 16:41:53 -0500

    added search history function and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,16 @@ var searchBar = document.querySelector("#city");
 var searchForm = document.querySelector("#search-form");
 var currentWeather = document.querySelector("#current-city");
 var forecastContainer = document.querySelector("#forecast");
+var searchHistory = document.querySelector("#search-history");
+var cityHistory = JSON.parse(localStorage.getItem("city-history"))||[]
 
 searchForm.addEventListener("submit", function(event){
     event.preventDefault();
     var city = searchBar.value.trim();
     getCityInfo(city);
-    searchBar.value = "";
     setLocalStorage(city);
+    searchBar.value = "";
+    
 })
 //add event listener to search bar to find the input text
 var getCityInfo = function(city){
@@ -19,14 +22,13 @@ var getCityInfo = function(city){
         
     })
     .then(function(data){
-        console.log(data);
         var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${data[0].lat}&lon=${data[0].lon}&exclude=hourly,minutely,alerts&appid=1ee273d29337b74fa7d7a2d9da75ae71`;
         fetch(apiUrl)
         .then(function(response){
             return response.json();
         })
         .then(function(data){
-            console.log(data);
+            document.getElementById("current-city").innerHTML = "";
             var tempElement = document.createElement("p")
             tempElement.textContent = `Max Temperature: ${data.current.temp}` //+ data.current.temp;
             var humidityElement = document.createElement("p")
@@ -38,6 +40,7 @@ var getCityInfo = function(city){
             document.getElementById("current-city").append(tempElement, humidityElement, windElement, uvElement);
 
             for(var i =1; i < 6; i++){
+                document.getElementById(i).innerHTML = "";
                 var tempElement = document.createElement("p")
                 tempElement.textContent = "Predicted Temperature: " + data.daily[i].temp.day;
                 var humidityElement = document.createElement("p")
@@ -53,7 +56,27 @@ var getCityInfo = function(city){
     //create local storage
  
 }
-setLocalStorage = function(data){
-    localStorage.setItem(data.name, data.name);
-}; 
+setLocalStorage = function(city){
+    if(cityHistory.indexOf(city) === -1){
+        cityHistory.push(city);
+        localStorage.setItem("city-history", JSON.stringify(cityHistory));
+        showHistory();
+    };
+    }
+showHistory = function () {
+    searchHistory.innerHTML = "";
+    for(var i = 0; i < cityHistory.length; i++){
+        var button = document.createElement("button");
+        button.textContent = cityHistory[i];
+        button.type = "button";
+        button.addEventListener("click",function (event) {
+            var city = event.target.innerText;
+            getCityInfo(city);
+          })
+        searchHistory.append(button);
+    }
+  }
+  showHistory();
+
+//
 // getCityInfo("london");
