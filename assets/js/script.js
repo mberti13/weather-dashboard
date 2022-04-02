84bc7079b3d24f7b328ab33888fd64c0e6ff0ef3
MJB <matt.berti13@gmail.com>
2022-04-01 23:03:58 -0500

    added weather icon and city name to current city section

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,6 @@ var getCityInfo = function(city){
     fetch(currentApiUrl)
     .then(function(response){
         return response.json();
-        
     })
     .then(function(data){
         var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${data[0].lat}&lon=${data[0].lon}&exclude=hourly,minutely,alerts&appid=1ee273d29337b74fa7d7a2d9da75ae71`;
@@ -28,16 +27,32 @@ var getCityInfo = function(city){
             return response.json();
         })
         .then(function(data){
+            console.log(data);
+            //resets current city form on reload
             document.getElementById("current-city").innerHTML = "";
+            //create h3 and display city name
+            var currentCityName = document.createElement("h2");
+            currentCityName.textContent = `${city}`;
+            //create h4 and display date
+            var currentDateEl = document.createElement("h4");
+            currentDateEl.innerHTML = moment().format("dddd, MMMM Do");
+            //create img and display Icon
+            var weatherIconEl = document.createElement("img");
+            weatherIconEl.setAttribute('src', 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '.png');
+            //create p and display Max Temp
             var tempElement = document.createElement("p")
             tempElement.textContent = `Max Temperature: ${data.current.temp}` //+ data.current.temp;
+            //create p and display Humidity
             var humidityElement = document.createElement("p")
             humidityElement.textContent = "Humidity: " + data.current.humidity;
+            //create p and display Wind Speed           
             var windElement = document.createElement("p");
             windElement.textContent = "Wind Speed: " + data.current.wind_speed;
+            //create p and display UV Index
             var uvElement = document.createElement("p");
             uvElement.textContent = "U.V. Index: "+ data.current.uvi;
-            document.getElementById("current-city").append(tempElement, humidityElement, windElement, uvElement);
+            //Append all to #current-city section
+            document.getElementById("current-city").append(currentCityName, weatherIconEl, currentDateEl, tempElement, humidityElement, windElement, uvElement);
 
             for(var i =1; i < 6; i++){
                 document.getElementById(i).innerHTML = "";
